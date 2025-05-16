eb601c1ee3a3f1fd73752a1b2d11c95e45f4b078
Christian Hugo Rasmussen <chur@itu.dk>
2025-05-16 11:44:39 +0000

    16 May 2025, 13:44

--- scripts/models.py
+++ scripts/models.py
@@ -3,11 +3,10 @@ import torch.nn.functional as F
 from torch_geometric.nn import GCNConv, GATConv
 
 class GCN(torch.nn.Module):
-    def __init__(self, hidden_channels, num_layers, random_seed, dropout_p, bins, data):
+    def __init__(self, hidden_channels, num_layers, random_seed, bins, data):
         super().__init__()
         torch.manual_seed(random_seed)
         self.input_layer = GCNConv(data.num_features, hidden_channels, improved=True, cached=True)
-        self.dropout_p = dropout_p
         # Create intermediate hidden layers (optional)
         self.hidden_layers = torch.nn.ModuleList()
         for _ in range(num_layers):
@@ -21,23 +20,22 @@ class GCN(torch.nn.Module):
     def forward(self, x, edge_index):
         x = self.input_layer(x, edge_index)
         x = F.relu(x)
-        x = F.dropout(x, p=self.dropout_p, training=self.training)
+        x = F.dropout(x, p=0.5, training=self.training)
 
         for layer in self.hidden_layers:
             x = layer(x, edge_index)
             x = F.relu(x)
-            x = F.dropout(x, p=self.dropout_p, training=self.training)
+            x = F.dropout(x, p=0.5, training=self.training)
 
         x = self.output_layer(x, edge_index)
         return x
 
 class GAT(torch.nn.Module):
-    def __init__(self, hidden_channels, num_layers, random_seed, dropout_p, bins, data, num_heads):
+    def __init__(self, hidden_channels, num_layers, random_seed, bins, data, num_heads):
         super().__init__()
         torch.manual_seed(random_seed)  # Replace with your desired seed
 
         self.convs = torch.nn.ModuleList()
-        self.dropout_p = dropout_p
 
         # Input layer
         self.convs.append(GATConv(data.num_features, hidden_channels, heads=num_heads, concat=True))
@@ -56,17 +54,16 @@ class GAT(torch.nn.Module):
         for conv in self.convs[:-1]:
             x = conv(x, edge_index)
             x = F.elu(x)
-            x = F.dropout(x, p=self.dropout_p, training=self.training)  # Adjust dropout probability as needed
+            x = F.dropout(x, p=0.5, training=self.training)  # Adjust dropout probability as needed
 
         x = self.convs[-1](x, edge_index)
         return x
 
 class MLP(torch.nn.Module):
-    def __init__(self, hidden_channels, num_layers, random_seed, dropout_p, bins, data, num_heads = None):
+    def __init__(self, hidden_channels, num_layers, random_seed, bins, data, num_heads = None):
         super().__init__()
         torch.manual_seed(random_seed)
         self.fcs = torch.nn.ModuleList()
-        self.dropout_p = dropout_p
         self.fcs.append(torch.nn.Linear(data.num_features, hidden_channels))
         for _ in range(num_layers):
             self.fcs(torch.nn.Linear(hidden_channels, hidden_channels))
@@ -78,6 +75,6 @@ class MLP(torch.nn.Module):
         for fc in self.fcs[:-1]:
             x = fc(x)
             x = F.elu(x)
-            x = F.dropout(x, p = self.dropout_p, training = self.training)
+            x = F.dropout(x, p = 0.5, training = self.training)
         x = self.fcs[-1](x)
         return x
